27d2dc49afbda3de150dfa485308b9373f4b289a
Lucas Loreggia <lloreggia@gmail.com>
2018-12-27 22:41:13 +0100

    Added visualizer settings

---
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/Shadow/ShadowVisualizerSettings.cs
@@ -0,0 +1,9 @@
+namespace UnityEngine.Experimental.Rendering.HDPipeline
+{
+    public class ShadowVisualizerSettings
+    {
+        public static int IndexOffset;
+
+        public static Vector2? SelectedScreenCoords;
+    }
+}
